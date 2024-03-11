72807a472ac696c8ed4becac20cfc1b3d0ce892a
Igor <igor.nacimento51@gmail.com>
2024-03-11 13:42:01 -0300

    "feat: add backend integration"

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -2,19 +2,20 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { MessageResponse } from '../types/message-response.type';
+import { env } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
 
-  private endpointUrl = '';
+  private endpointUrl = env.apiUrl;
 
   constructor(private http: HttpClient) { }
 
   send(message:string): Observable<MessageResponse>{
     const data = { message };
 
-    return this.http.post<MessageResponse>(this.endpointUrl, data);
+    return this.http.post<MessageResponse>(env.apiUrl, data);
   }
 }
